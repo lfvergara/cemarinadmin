79b3f3af05ba83d350434fe04a4c2dc913276a99
Federico Vergara <hu.ce.ro@gmail.com>
2022-03-23 19:08:50 -0300

    Subiendo investigador

---
+++ modules/investigador/controller.php
@@ -0,0 +1,48 @@
+<?php
+//require_once "modules/investigador/model.php";
+require_once "modules/investigador/view.php";
+require_once "modules/investigadorcategoria/model.php";
+require_once "modules/lineainvestigacion/model.php";
+require_once "modules/sublineainvestigacion/model.php";
+require_once "modules/universidad/model.php";
+require_once "modules/universidadfacultad/model.php";
+require_once "modules/universidadarea/model.php";
+
+
+class InvestigadorController {
+
+	function __construct() {
+		//$this->model = new Investigador();
+		$this->view = new InvestigadorView();
+	}
+
+	function agregar() {
+    	SessionHandler()->check_session();
+		//SessionHandler()->check_admin_level();
+		$investigadorcategoria_collection = Collector()->get('InvestigadorCategoria');
+		$lineainvestigacion_collection = Collector()->get('LineaInvestigacion');
+		$sublineainvestigacion_collection = Collector()->get('SubLineaInvestigacion');
+		$universidad_collection = Collector()->get('Universidad');
+		$universidadfacultad_collection = Collector()->get('UniversidadFacultad');
+		$universidadarea_collection = Collector()->get('UniversidadArea');
+		$this->view->agregar($investigadorcategoria_collection, $lineainvestigacion_collection, $sublineainvestigacion_collection, $universidad_collection, $universidadfacultad_collection, $universidadarea_collection);
+	}
+
+	function guardar() {
+		SessionHandler()->check_session();
+		//SessionHandler()->check_admin_level();		
+		foreach ($_POST as $key=>$value) $this->model->$key = $value;
+        $this->model->save();
+		header("Location: " . URL_APP . "/investigadorcategoria/panel");
+	}
+
+	function editar($arg) {
+		SessionHandler()->check_session();
+		//SessionHandler()->check_admin_level();
+		$this->model->investigadorcategoria_id = $arg;
+		$this->model->get();
+		$investigadorcategoria_collection = Collector()->get('InvestigadorCategoria');
+		$this->view->editar($investigadorcategoria_collection, $this->model);
+	}
+}
+?>
---
+++ modules/investigador/view.php
@@ -0,0 +1,44 @@
+<?php
+
+
+class InvestigadorView extends View {
+
+	function agregar($investigadorcategoria_collection, $lineainvestigacion_collection, $sublineainvestigacion_collection, $universidad_collection, $universidadfacultad_collection, $universidadarea_collection) {
+		$gui = file_get_contents("static/modules/investigadorcategoria/agregar.html");
+		$gui_slt_investigadorcategoria = file_get_contents("static/common/slt_investigadorcategoria.html");
+		$gui_slt_investigadorcategoria = $this->render_regex('SLT_INVESTIGADORCATEGORIA', $gui_slt_investigadorcategoria, $investigadorcategoria_collection);
+		$gui_slt_lineainvestigacion = file_get_contents("static/common/slt_lineainvestigacion.html");
+		$gui_slt_lineainvestigacion = $this->render_regex('SLT_LINEAINVESTIGACION', $gui_slt_lineainvestigacion, $lineainvestigacion_collection);
+		$gui_slt_sublineainvestigacion = file_get_contents("static/common/slt_sublineainvestigacion.html");
+		$gui_slt_sublineainvestigacion = $this->render_regex('SLT_SUBLINEAINVESTIGACION', $gui_slt_sublineainvestigacion, $sublineainvestigacion_collection);
+		$gui_slt_universidad = file_get_contents("static/common/slt_universidad.html");
+		$gui_slt_universidad = $this->render_regex('SLT_UNIVERSIDAD', $gui_slt_universidad, $universidad_collection);
+		$gui_slt_universidadfacultad = file_get_contents("static/common/slt_universidadfacultad.html");
+		$gui_slt_universidadfacultad = $this->render_regex('SLT_UNIVERSIDADFACULTAD', $gui_slt_universidadfacultad, $universidadfacultad_collection);
+		$gui_slt_universidadarea = file_get_contents("static/common/slt_universidadarea.html");
+		$gui_slt_universidadarea = $this->render_regex('SLT_UNIVERSIDADAREA', $gui_slt_universidadarea, $universidadarea_collection);
+
+		$render = str_replace('{slt_investigadorcategoria}', $gui_slt_investigadorcategoria, $gui);
+		$render = str_replace('{slt_lineainvestigacion}', $gui_slt_lineainvestigacion, $render);
+		$render = str_replace('{slt_sublineainvestigacion}', $gui_slt_sublineainvestigacion, $render);
+		$render = str_replace('{slt_universidad}', $gui_slt_universidad, $render);
+		$render = str_replace('{slt_universidadfacultad}', $gui_slt_universidadfacultad, $render);
+		$render = str_replace('{slt_universidadarea}', $gui_slt_universidadarea, $render);
+		$render = $this->render_breadcrumb($render);
+		$template = $this->render_template($render);
+		print $template;
+	}
+
+	function editar($investigadorcategoria_collection, $obj_investigadorcategoria) {
+		$gui = file_get_contents("static/modules/investigadorcategoria/editar.html");
+		$gui_tbl_investigadorcategoria = file_get_contents("static/common/tbl_investigadorcategoria.html");
+		$gui_tbl_investigadorcategoria = $this->render_regex('TBL_INVESTIGADORCATEGORIA', $gui_tbl_investigadorcategoria, $investigadorcategoria_collection);
+		$obj_investigadorcategoria = $this->set_dict($obj_investigadorcategoria);
+		$render = str_replace('{tbl_investigadorcategoria}', $gui_tbl_investigadorcategoria, $gui);
+		$render = $this->render($obj_investigadorcategoria, $render);
+		$render = $this->render_breadcrumb($render);
+		$template = $this->render_template($render);
+		print $template;
+	}
+}
+?>
